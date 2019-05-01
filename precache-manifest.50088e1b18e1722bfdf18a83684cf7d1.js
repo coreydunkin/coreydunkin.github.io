2dbc017ec28d37ca27047fb207264149b7accd21
Corey Dunkin <corey.dunkin@gmail.com>
2019-05-01 12:15:49 +1000

    v2

---
+++ precache-manifest.50088e1b18e1722bfdf18a83684cf7d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc04a3e0fc4928a95c1e21200922d70b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81cc06d0c15f3b0bb629",
+    "url": "/static/css/main.06746b13.chunk.css"
+  },
+  {
+    "revision": "cb94e48007fbf7530563",
+    "url": "/static/js/2.d91edcc4.chunk.js"
+  },
+  {
+    "revision": "81cc06d0c15f3b0bb629",
+    "url": "/static/js/main.6d5677be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
