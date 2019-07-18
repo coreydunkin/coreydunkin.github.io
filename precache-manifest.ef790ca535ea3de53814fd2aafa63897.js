2b7c7f6c6e958cba030ff1bb186c08bf1688b5fb
Corey Dunkin <corey.dunkin@gmail.com>
2019-07-18 11:31:50 +1000

    new static build revision

---
+++ precache-manifest.ef790ca535ea3de53814fd2aafa63897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c24ffe0601db4350c7d1ef0bc5191fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a3db05e85f1f7ab64d5",
+    "url": "/static/css/main.a186e3f5.chunk.css"
+  },
+  {
+    "revision": "b35af334453a2ba24ba7",
+    "url": "/static/js/2.b4b52202.chunk.js"
+  },
+  {
+    "revision": "7a3db05e85f1f7ab64d5",
+    "url": "/static/js/main.96be7817.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.499e4ea4dad10ad043aa21ab6bec6b2d.js"
+  "/precache-manifest.ef790ca535ea3de53814fd2aafa63897.js"
 );
 
 self.addEventListener('message', (event) => {
